d631cf05a07c61923b7e6079242d954700cf3716
Anupam.Sharnagat <anupam.sharnagat@ing.com>
2023-02-23 17:10:52 +0100

    first commit

---
+++ src/com/learn/java/lambdas/RunnableLambdaExample.java
@@ -0,0 +1,10 @@
+package com.learn.java.lambdas;
+
+public class RunnableLambdaExample {
+    public static void main(String[] args) {
+        /***
+         * Prior to java 8
+         * **/
+
+    }
+}
