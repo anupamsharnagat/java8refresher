df0e76aa57214e05a12e94d9cee2b2a5c76d1713
Anupam.Sharnagat <anupam.sharnagat@ing.com>
2023-02-23 23:00:44 +0100

    "Committing runnable example"

--- src/com/learn/java/lambdas/RunnableLambdaExample.java
+++ src/com/learn/java/lambdas/RunnableLambdaExample.java
@@ -5,6 +5,27 @@ public class RunnableLambdaExample {
         /***
          * Prior to java 8...
          * **/
+        Runnable runnable=new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("inside runnable 1");
+            }
+        };
+
+        new Thread(runnable).start();
+
+        //using lambda
+
+        Runnable runnable1=()->{
+            System.out.println("inside Runnable 2");
+        };
+
+        Runnable runnable2=()-> System.out.println("inside runnable 3");
+        new Thread(runnable2).start();
+
+        new Thread(runnable1).start();
+
+        new Thread(()-> System.out.println("inside runnable 4")).start();
 
     }
 }
